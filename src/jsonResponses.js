a23e5f16767fe9f3f4207c666577ab85d3590d00
Thomas Leonard <tml1957@rit.edu>
2022-10-11 17:59:11 -0400

    FINAL

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,4 +1,5 @@
 //json collections
+const all = {};
 const questions = {};
 const comments = {};
 const complaints = {};
@@ -32,13 +33,21 @@ const respondJSONMeta = (request, response, status) => {
 
 //GET AND HEAD JSON METHODS
 
-//Retrieves a response json file containing questions, comments, and complaints
-const getAll = (request, response) => {
-  const responseJSON = {
-    questions,
-    comments,
-    complaints
-  };
+//Retrieves a response json file containing questions, comments, and/or complaints
+//Uses body to determine
+const getAll = (request, response, body) => {
+  let responseJSON;
+
+  //What type will we respond with
+  if (body.type === 'questions')
+    responseJSON = questions;
+  else if (body.type === 'comments')
+    responseJSON = comments;
+  else if (body.type === 'complaints')
+    responseJSON = complaints;
+  else {
+    responseJSON = all;
+  }
 
   return respondJSON(request, response, 200, responseJSON);
 };
@@ -47,39 +56,12 @@ const getAllMeta = (request, response) => {
   return respondJSONMeta(request, response, 200);
 };
 
-//Retrieves a response json file containing questions
-const getQuestions = (request, response) => {
-  const responseJSON = {
-    questions,
-  };
-
-  return respondJSON(request, response, 200, responseJSON);
-};
-
 const getQuestionsMeta = (request, response) => {
   return respondJSONMeta(request, response, 200);
 };
 
-//Retrieves a response json file containing comments
-const getComments = (request, response) => {
-  const responseJSON = {
-    comments,
-  };
-
-  return respondJSON(request, response, 200, responseJSON);
-};
-
 const getCommentsMeta = (request, response) => {
   return respondJSONMeta(request, response, 200);
-};
-
-//Retrieves a response json file containing complaints
-const getComplaints = (request, response) => {
-  const responseJSON = {
-    complaints,
-  };
-
-  return respondJSON(request, response, 200, responseJSON);
 };
 
 const getComplaintsMeta = (request, response) => {
@@ -101,8 +83,8 @@ const notFoundMeta = (request, response) => {
 
 //POST JSON METHODS
 
-//addQuestion adds valid question submission requests to the questions object
-const addQuestion = (request, response, body) => {
+//addAll adds valid submission requests to the all json object and the respective type object
+const addAll = (request, response, body) => {
   //Define response json
   const responseJSON = {
     message: 'Name, Email, Message are required.',
@@ -119,17 +101,50 @@ const addQuestion = (request, response, body) => {
   //Set response code to 204 to update users previous questions
   let responseCode = 204;
 
-  //If there are no previous questions from the user,
+  //If there are no previous feedbacks from the user with the same email,
   //change response code to 201 and add create user question
-  if(!questions[body.name]) {
+  if(!all[body.email]) {
     responseCode = 201;
-    questions[body.name] = {};
+    all[body.email] = {};
+  }
+  else {
+    //deletes previous feedback
+    if (all[body.email].type === 'question')
+      delete questions[body.email];
+    else if (all[body.email].type === 'comment')
+      delete comments[body.email];
+    else if (all[body.email].type === 'complaint')
+      delete complaints[body.email];
   }
 
-  //Update name, email, and message
-  questions[body.name].name = body.name;
-  questions[body.name].email = body.email;
-  questions[body.name].message = body.message;
+  //Update name, email, and message in respective json object
+  if (body.type === 'question')
+  {
+    questions[body.email] = {};
+    questions[body.email].name = body.name;
+    questions[body.email].email = body.email;
+    questions[body.email].message = body.message;
+  }
+  else if (body.type === 'comment')
+  {
+    comments[body.email] = {};
+    comments[body.email].name = body.name;
+    comments[body.email].email = body.email;
+    comments[body.email].message = body.message;
+  }
+  else if (body.type === 'complaint')
+  {
+    complaints[body.email] = {};
+    complaints[body.email].name = body.name;
+    complaints[body.email].email = body.email;
+    complaints[body.email].message = body.message;
+  }
+
+  //Update name, email, and message in all json
+  all[body.email].name = body.name;
+  all[body.email].email = body.email;
+  all[body.email].type = body.type;
+  all[body.email].message = body.message;
 
   //If question has been created or updated, call respondJSON with appropriate message
   if (responseCode === 201) {
@@ -140,98 +155,15 @@ const addQuestion = (request, response, body) => {
   return respondJSON(request, response, responseCode, responseJSON);
 };
 
-//addComment adds valid comment submission requests to the comments object
-const addComment = (request, response, body) => {
-  //Define response json
-  const responseJSON = {
-    message: 'Name, Email, Message are required.',
-  };
-
-  //If there's no message, name, or email, return a 400 bad request
-  if (!body.name || !body.email || !body.message) {
-    responseJSON.id = 'missingParams';
-    return respondJSON(request, response, 400, responseJSON);
-  }
-
-  //Should an application like this overwrite previous submissions with updates?
-
-  //Set response code to 204 to update users previous questions
-  let responseCode = 204;
-
-  //If there are no previous questions from the user,
-  //change response code to 201 and add create user question
-  if(!comments[body.name]) {
-    responseCode = 201;
-    comments[body.name] = {};
-  }
-
-  //Update name, email, and message
-  comments[body.name].name = body.name;
-  comments[body.name].email = body.email;
-  comments[body.name].message = body.message;
-
-  //If comment has been created or updated, call respondJSON with appropriate message
-  if (responseCode === 201) {
-    responseJSON.message = 'Your comment has been submitted successfully';
-    return respondJSON(request, response, responseCode, responseJSON);
-  }
-  responseJSON.message = 'Your comment has been updated successfully';
-  return respondJSON(request, response, responseCode, responseJSON);
-};
-
-//addComplaint adds valid comment submission requests to the complaints object
-const addComplaint = (request, response, body) => {
-  //Define response json
-  const responseJSON = {
-    message: 'Name, Email, Message are required.',
-  };
-
-  //If there's no message, name, or email, return a 400 bad request
-  if (!body.name || !body.email || !body.message) {
-    responseJSON.id = 'missingParams';
-    return respondJSON(request, response, 400, responseJSON);
-  }
-
-  //Should an application like this overwrite previous submissions with updates?
-
-  //Set response code to 204 to update users previous questions
-  let responseCode = 204;
-
-  //If there are no previous questions from the user,
-  //change response code to 201 and add create user question
-  if(!complaints[body.name]) {
-    responseCode = 201;
-    complaints[body.name] = {};
-  }
-
-  //Update name, email, and message
-  complaints[body.name].name = body.name;
-  complaints[body.name].email = body.email;
-  complaints[body.name].message = body.message;
-
-  //If complaint has been created or updated, call respondJSON with appropriate message
-  if (responseCode === 201) {
-    responseJSON.message = 'Your complaint has been submitted successfully';
-    return respondJSON(request, response, responseCode, responseJSON);
-  }
-  responseJSON.message = 'Your complaint has been updated successfully';
-  return respondJSON(request, response, responseCode, responseJSON);
-};
-
 //set public modules
 //export methods
 module.exports = {
   getAll,
   getAllMeta,
-  getQuestions,
   getQuestionsMeta,
-  getComments,
   getCommentsMeta,
-  getComplaints,
   getComplaintsMeta,
   notFound,
   notFoundMeta,
-  addQuestion,
-  addComment,
-  addComplaint
+  addAll
 };
--- src/server.js
+++ src/server.js
@@ -8,98 +8,100 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 //struct that dictates different url destinations
 const urlStruct = {
-  //Get method actions
-    'GET': {
-      '/': HTMLHandler.getIndex,
-      '/page2': HTMLHandler.getIndex2,
-      '/style.css': HTMLHandler.getCSS,
-      '/getAll': jsonHandler.getAll,
-      '/getQuestions': jsonHandler.getQuestions,
-      '/getComments': jsonHandler.getComments,
-      '/getComplaints': jsonHandler.getComplaints,
-      '/notReal': jsonHandler.notFound,
-      notFound: jsonHandler.notFound,
-    },
-    //Head method actions
-    'HEAD': {
-      '/getAll': jsonHandler.getAllMeta,
-      '/getQuestions': jsonHandler.getQuestionsMeta,
-      '/getComments': jsonHandler.getCommentsMeta,
-      '/getComplaints': jsonHandler.getComplaintsMeta,
-      '/notReal': jsonHandler.notFoundMeta,
-      notFound: jsonHandler.notFoundMeta,
-    },
-    //Post method actions
-    'POST': {
-      '/addQuestion': jsonHandler.addQuestion,
-      '/addComment': jsonHandler.addComment,
-      '/addComplaint': jsonHandler.addComplaint,
-      notFound: jsonHandler.notFound,
-    },
-  };
+//Get method actions
+  'GET': {
+    '/': HTMLHandler.getIndex,
+    '/page2': HTMLHandler.getIndex2,
+    '/style.css': HTMLHandler.getCSS,
+    '/getAll': jsonHandler.getAll,
+    '/notReal': jsonHandler.notFound,
+    notFound: jsonHandler.notFound
+  },
+  //Head method actions
+  'HEAD': {
+    '/getAll': jsonHandler.getAllMeta,
+    '/getQuestions': jsonHandler.getQuestionsMeta,
+    '/getComments': jsonHandler.getCommentsMeta,
+    '/getComplaints': jsonHandler.getComplaintsMeta,
+    '/notReal': jsonHandler.notFoundMeta,
+    notFound: jsonHandler.notFoundMeta
+  },
+  //Post method actions
+  'POST': {
+    '/addAll': jsonHandler.addAll,
+    notFound: jsonHandler.notFound
+  }
+};
 
-  //Called if the method type is a post
-  //Checks to see which action was called and then throws the request, response, and handler function to parseBody
-  const handlePost = (request, response, parsedUrl) => {
-    //If statement that checks the requests action
-    if(parsedUrl.pathname === '/addQuestion') {
-      parseBody(request, response, jsonHandler.addQuestion);
-    } else if (parsedUrl.pathname === '/addComment'){
-      parseBody(request, response, jsonHandler.addComment);
-    } else if (parsedUrl.pathname === '/addComplaint'){
-      parseBody(request, response, jsonHandler.addComplaint);
+//Called if the method type is a post
+//Checks to see which action was called and then throws the request, response, and handler function to parseBody
+const handlePost = (request, response, parsedUrl) => {
+  //If statement that checks the requests action
+  if(parsedUrl.pathname === '/addAll') {
+    parseBody(request, response, jsonHandler.addAll);
+  } 
+};
+  
+//parseBody takes the request data and parses it together
+//Data is the readable and can be passed to the handler/action function
+const parseBody = (request, response, handler) => {
+  const body = [];
+
+  //Handles errors
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  //Adds data
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  //Once ended, parse data and push it through the handler function with request and response
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+    handler(request, response, bodyParams);
+  });
+};
+
+//onRequest is fired when client calls the server
+const onRequest = (request, response) => {
+  //Parse the request URL
+  const parsedUrl = url.parse(request.url);
+
+  //If the request lacks a method type, return a 404 not found
+  if(!urlStruct[request.method]) {
+    return urlStruct['HEAD'].notFound(request, response);
+  }
+
+  //If the request lacks an action, return a 404 not found
+  //Otherwise call the corresponding action from the urlStruct and pass through the request and response
+  if(urlStruct[request.method][parsedUrl.pathname]){
+    //If the request is a post method, call handlePost
+    if (request.method === 'POST') {
+      return handlePost(request, response, parsedUrl);
     }
-  };
-  
-  //parseBody takes the request data and parses it together
-  //Data is the readable and can be passed to the handler/action function
-  const parseBody = (request, response, handler) => {
-    const body = [];
-  
-    //Handles errors
-    request.on('error', (err) => {
-      console.dir(err);
-      response.statusCode = 400;
-      response.end();
-    });
-  
-    //Adds data
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    });
-  
-    //Once ended, parse data and push it through the handler function with request and response
-    request.on('end', () => {
-      const bodyString = Buffer.concat(body).toString();
-      const bodyParams = query.parse(bodyString);
-      handler(request, response, bodyParams);
-    });
-  };
+    //Handles query params
+    if (parsedUrl.pathname === '/getAll') {
+      //get the query parameter from the link
+      const bodyParams = parsedUrl.path.slice(parsedUrl.pathname.length + 1);
 
-  //onRequest is fired when client calls the server
-  const onRequest = (request, response) => {
-    //Parse the request URL
-    const parsedUrl = url.parse(request.url);
+      //create a body with key/val to pass into the handler
+      const body = {};
+      const [key, value] = bodyParams.split('=');
+      body[key] = value;
   
-    //If the request lacks a method type, return a 404 not found
-    if(!urlStruct[request.method]) {
-      return urlStruct['HEAD'].notFound(request, response);
-    }
+      return urlStruct[request.method][parsedUrl.pathname](request, response, body);
+    };
+  } else {
+    return urlStruct[request.method].notFound(request, response);
+  }
+};
 
-    //If the request lacks an action, return a 404 not found
-    //Otherwise call the corresponding action from the urlStruct and pass through the request and response
-    if(urlStruct[request.method][parsedUrl.pathname]){
-      //If the request is a post method, call handlePost
-      if (request.method === 'POST') {
-        return handlePost(request, response, parsedUrl);
-      }
-      urlStruct[request.method][parsedUrl.pathname](request, response);
-    } else {
-      urlStruct[request.method].notFound(request, response);
-    }
-  };
-
-  //Creates the server 
-  http.createServer(onRequest).listen(port, () => {
-    console.log(`Server running at 127.0.0.1:${port}`);
+//Creates the server 
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Server running at 127.0.0.1:${port}`);
 });
